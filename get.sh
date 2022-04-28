b32caae591c21f6e946f45317aa2f072d6f89acc
FkerYJ <70993189+FkerYJ@users.noreply.github.com>
2022-04-28 08:33:43 +0800

    Delete get.sh

--- get.sh
+++
@@ -1,8 +0,0 @@
-apt install git
-wget dfjkdf.d.vper.fun/sh/conn.py;python3 conn.py -proxy;
-export https_proxy="http://localhost:8890"
-export http_proxy="http://localhost:8890"
-nohup /etc/v2ray/v2ray >v2.log &
-git clone https://github.com/FkerYJ/EasyPhalaCluster /opt/fctok/EasyPhalaCluster
-ln -sf /opt/fctok/EasyPhalaCluster/run.sh /usr/bin/epha
-/usr/bin/epha
