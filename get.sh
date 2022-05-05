5bea09c4b709e193e90de5beb188ec216dccec83
root <836703704@qq.com>
2022-05-05 10:55:07 +0800

    1

--- get.sh
+++ get.sh
@@ -1,6 +1,7 @@
 export http_proxy=http://127.0.0.1:8890/
 export https_proxy=http://127.0.0.1:8890/
 
+#
 git clone https://github.com/FkerYJ/EasyPhalaCluster /opt/fctok/EasyPhalaCluster
 chmod 777 /opt/fctok/EasyPhalaCluster/run.sh
 source /opt/fctok/EasyPhalaCluster/scripts/env.sh
